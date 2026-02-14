4e44dfddd2775132a2e247e71e40a5eb0245c964
JershaTumalad <tahtianajershatumalad@gmail.com>
2026-02-14 17:23:30 +0800

    Initial commit

---
+++ OneAccountManagement/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OneAccountManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string UserName = "NameUserr";
+            string Password = "PassUser123";
+
+
+            Console.WriteLine("ACCOUNT MANAGEMENT SYSTEM");
+            Console.Write("Enter user name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter password: ");
+            string pass = Console.ReadLine();
+
+
+            if (UserName == name && Password == pass)
+            {
+                Console.WriteLine("Login Successful.");
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid input.");
+            }
+        }
+    }
+}
